576205ee447f5a1b47d06027cb83f349890f92c7
ewhalgand <halgandewen44@gmail.com>
2025-06-11 14:22:24 +0200

    add: film

--- Main.java
+++ Main.java
@@ -1,7 +1,10 @@
 public class Main {
   public static void main(String... strings) {
 
-    Film monFilm = new Film("Inception", "2010", "Christopher Nolan", Categories.FICTION.getDescription());
-    System.out.println(monFilm);
+    Film monFilm1 = new Film("Inception", "2010", "Christopher Nolan", Categories.FICTION.getDescription());
+
+    Film monFilm2 = new Film("Balerina", "2025", "unknown", Categories.FICTION.getDescription());
+    System.out.println(monFilm1);
+    System.out.println(monFilm2);
   }
 }
